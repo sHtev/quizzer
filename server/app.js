b21d81ea10593dd6ce21cf47c2566b52e43748a3
Steve Gilroy <stephen.gilroy@raspberrypi.org>
2020-05-17 03:43:20 +0100

    add https redirect

--- server/app.js
+++ server/app.js
@@ -31,9 +31,21 @@ const hbs = exphbs.create({
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(logger("dev"));
+app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === "production") {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect("https://" + req.headers.host + req.url);
+    } else {
+      return next();
+    }
+  } else {
+    return next();
+  }
+});
 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
